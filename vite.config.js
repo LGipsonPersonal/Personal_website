70bb17b5e582b1847ff642703bfc05b8bc470164
GipsonL <78498165+GipsonL@users.noreply.github.com>
2025-07-27 17:40:23 -0500

    fix for base url

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Use './' for relative paths
   plugins: [svelte()],
-})
+  resolve: {
+    alias: {
+      $lib: path.resolve(__dirname, "./src/lib"),
+      $assets: path.resolve(__dirname, "./src/assets")
+    },
+  },
