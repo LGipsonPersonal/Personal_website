74b0742564a1a952f5953fc159eb0fdd67d386be
GipsonL <78498165+GipsonL@users.noreply.github.com>
2025-07-27 17:41:32 -0500

    fix for prev commit

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   base: './', // Use './' for relative paths
   plugins: [svelte()],
-  resolve: {
-    alias: {
-      $lib: path.resolve(__dirname, "./src/lib"),
-      $assets: path.resolve(__dirname, "./src/assets")
-    },
-  },
+  })
